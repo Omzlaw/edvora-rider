c4d17695b9926fc116154cffe91b7e3908e49610
Omzlaw <omeizaalabi@gmail.com>
2022-05-05 12:57:10 +0100

    header details css change

--- src/components/header-user-details/useStyles.js
+++ src/components/header-user-details/useStyles.js
@@ -4,10 +4,11 @@ export const useStyles = {
     username: {
         fontWeight: 'bold',
         fontSize: '20px',
-        marginRight: '25px',
+        marginRight: '70px',
         display: 'flex',
         overflow: 'wrap',
         maxWidth: '30px',
+
     },
     avatar: {
         borderRadius: '50%',
