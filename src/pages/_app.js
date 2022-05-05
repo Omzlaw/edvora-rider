de5d9b0aaa8d93ff71e7e9140ae16abe7a452537
Omzlaw <omeizaalabi@gmail.com>
2022-05-05 12:47:56 +0100

    add error boundary to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import wrapper from '../redux/store'
+import Error from "next/error";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  if (pageProps.error) {
+    return <Error statusCode={pageProps.error.statusCode} title={pageProps.error.message} />;
+  }
+  return (
+    <Component {...pageProps} />
+  );
 }
 
 export default wrapper.withRedux(MyApp)
